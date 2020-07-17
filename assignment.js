227a309caad192ea57bca9bc00cc69453f16f4b9
irfathopi <opikhanctg@gmail.com>
2020-07-17 13:35:40 +0100

    commit

--- assignment.js
+++ assignment.js
@@ -21,3 +21,41 @@ console.log(yourFeetAmount, 'Mile')
  var totalQuebecYouNeed =  woodCalculator(12,4,3);
  console.log(totalQuebecYouNeed, 'Quebec Feet');
 
+
+// problem 3
+function brickCalculator(){
+    
+    oneToTen = 15 * 1000;
+    elevenToTwenty = 12 * 1000;
+    extra = 10 * 1000;
+
+    if ( oneToTen){
+        console.log(oneToTen)
+    }
+    else if (elevenToTwenty){
+        console.log(elevenToTwenty);
+    }
+        else{
+            console.log(extra);
+        } 
+    
+}
+
+
+
+
+
+//  problem 4
+function tinyFriend(names){
+    var tinyName = names[0];
+    for(var i = 0; i < names.length; i++){
+        var currentName = names[i];
+        if( currentName.length < tinyName.length){
+            tinyName = currentName;
+        }
+    }
+    return tinyName;
+}
+
+var smallestName = tinyFriend(['hossain','opi','ba','Irfath']);
+console.log(smallestName);
